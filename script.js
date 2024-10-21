608e6400c6d43e60b7e078975892e19fcbadc97d
Nelson Sales <132019512+Nelson1Aguiar@users.noreply.github.com>
2024-10-21 20:12:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,31 +4,59 @@ const button = document.getElementById("botaoSalva");
 const buttonTesta = document.getElementById("botaoTestaSenha");
 
 const qtdCertos = document.getElementById("qtdCertos");
+const senhaTeste = document.getElementById("senhaTentativa");
+const aviso = document.getElementById("Aviso");
 
 let senhaDefinida;
+
+senha.addEventListener("input", function() {
+    aviso.innerHTML = '';
+
+    let senhaTesteComparacao = senha.value;
+
+    if (!isNaN(senhaTesteComparacao) && Number(senhaTesteComparacao) > 9999) {
+        aviso.innerHTML = "Máximo 4 dígitos";
+        senha.value = senhaTesteComparacao.slice(0, -1);
+    }
+});
+
+senhaTeste.addEventListener("input", function() {
+    qtdCertos.innerHTML = '';
+
+    let senhaTesteComparacao = senhaTeste.value;
+
+    if (!isNaN(senhaTesteComparacao) && Number(senhaTesteComparacao) > 9999) {
+        qtdCertos.innerHTML = "Máximo 4 dígitos";
+        senhaTeste.value = senhaTesteComparacao.slice(0, -1);
+    }
+});
+
 button.addEventListener("click",function(){
-    senhaDefinida = senha.value;
+    senhaDefinida = senha.value
+    if(Number(senhaDefinida)<1000){
+        aviso.innerHTML = "Mínimo 4 dígitos"
+        return;
+    }
     const testaSenhaDiv = document.getElementById("TestaSenha");
     const DefineSenhaDiv = document.getElementById("DefineSenha");
     DefineSenhaDiv.style.display = "none";
-    testaSenhaDiv.style.display = "block";
+    testaSenhaDiv.style.display = "flex";
 })
 
 buttonTesta.addEventListener("click",function(){
-    let senhaTeste = document.getElementById("senhaTentativa").value;
+    let senhaTesteComparacao = senhaTeste.value
+
+    if(Number(senhaTesteComparacao)<1000){
+        qtdCertos.innerHTML = "Mínimo 4 digitos"
+        return;
+    }
+
     let certos = 0;
 
-   if(senhaDefinida[0] === senhaTeste[0]){
-        certos+=1;
-    }
-    if(senhaDefinida[1] === senhaTeste[1]){
-        certos+=1;
-    }
-    if(senhaDefinida[2] === senhaTeste[2]){
-        certos+=1;
-    }
-    if(senhaDefinida[3] === senhaTeste[3]){
-        certos+=1;
+    for(let i=0; i<senhaTesteComparacao.length;i++){
+        if(senhaDefinida[i] === senhaTesteComparacao[i]){
+            certos+=1;
+        }
     }
     if(certos === 1){
         qtdCertos.innerHTML = certos + " Certo"
